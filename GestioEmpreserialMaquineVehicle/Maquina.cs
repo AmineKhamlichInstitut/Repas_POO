4b8e300277e595a893200aac427c22b901367f3c
AmineKhamlich <sk8.amine@gmail.com>
2025-10-07 17:54:45 +0200

    Afegir constructor

--- GestioEmpreserialMaquineVehicle/Maquina.cs
+++ GestioEmpreserialMaquineVehicle/Maquina.cs
@@ -8,6 +8,26 @@ namespace GestioEmpreserialMaquineVehicle
 {
     internal class Maquina
     {
-        public Maquina() { }
+        private string _nom;
+        private int _id;
+        private bool _reservada;
+        private string _produccioTipus;
+        public Maquina() 
+        {
+            _nom = string.Empty;
+            _id = 0;
+            _reservada = false;
+            _produccioTipus = string.Empty;
+        }
+
+        public Maquina(string nom, int id, bool reservada, string produccioTipus)
+        {
+            _nom = nom;
+            _id = id;
+            _reservada = reservada;
+            _produccioTipus = produccioTipus;
+        }
+
+
     }
 }
